ca9d30315427c1bbe4bd1ba2ee1df64d6fc0c836
Julius Boateng <jboateng@juliuss-mbp.dhcp.nd.edu>
2020-10-27 15:21:39 -0400

    Path drawing

--- scribble_app/lib/main.dart
+++ scribble_app/lib/main.dart
@@ -21,13 +21,14 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        primaryColor: Colors.red,
         // primaryColor: Colors.grey[900],
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Canvas'),
     );
   }
 }
@@ -50,29 +51,62 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class Path extends CustomPainter {
+  List<Offset> points;
+  Path({this.points});
+
+  Color color = Colors.blue;
+  double strokeWidth = 4;
+  StrokeCap strokeType = StrokeCap.round;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = color
+      ..strokeWidth = strokeWidth
+      ..strokeCap = strokeType;
+
+    for (int index = 0; index < points.length - 1; index++) {
+      if (points[index] != null && points[index + 1] != null) {
+        canvas.drawLine(points[index], points[index + 1], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(Path oldDelegate) => true;
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int scale = 12; //Scale of Screen
   double startX = 0;
   double startY = 0;
   int dx = 0;
   int dy = 0;
-  var data = [];
+  List data = [];
+  List<Offset> points = [];
 
   void _onPanStartHandler(DragStartDetails details) {
     setState(() {
-      startX = details.globalPosition.dx;
-      startY = details.globalPosition.dy;
-      this.dx = 0;
-      this.dy = 0;
-      this.data = ["$dx:$dy"];
+      this.points = [];
+      points.add(details.localPosition);
+
+      // RenderBox renderBox = context.findRenderObject();
+      // points.add(renderBox.globalToLocal(details.globalPosition));
+      // startX = details.globalPosition.dx;
+      // startY = details.globalPosition.dy;
+      // this.dx = 0;
+      // this.dy = 0;
+      // this.data = ["$dx:$dy"];
     });
   }
 
   void _onPanUpdateHandler(DragUpdateDetails details) {
     setState(() {
-      this.dx = ((startX - details.globalPosition.dx) / scale).floor();
-      this.dy = ((startY - details.globalPosition.dy) / scale).floor();
-      this.data.add("$dx:$dy");
+      points.add(details.localPosition);
+      // this.dx = ((startX - details.globalPosition.dx) / scale).floor();
+      // this.dy = ((startY - details.globalPosition.dy) / scale).floor();
+      // this.data.add("$dx:$dy");
     });
   }
 
@@ -84,21 +118,29 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return GestureDetector(
-        onPanStart: _onPanStartHandler,
-        onPanUpdate: _onPanUpdateHandler,
-        child: Scaffold(
-            backgroundColor: Colors.grey[900],
 
-            // appBar: AppBar(
-            //   // Here we take the value from the MyHomePage object that was created by
-            //   // the App.build method, and use it to set our appbar title.
-            //   title: Text(widget.title),
-            // ),
-            body: Center(
-                child: Text(
-              "$dx, $dy\n $data",
-              style: TextStyle(color: Colors.white),
-            ))));
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: GestureDetector(
+            onPanStart: _onPanStartHandler,
+            onPanUpdate: _onPanUpdateHandler,
+            child: Container(
+                color: Colors.grey[900],
+                child: Stack(
+                  children: <Widget>[
+                    Center(
+                        child: Text(
+                      // "$dx, $dy\n $data",
+                      "$points",
+                      style: TextStyle(color: Colors.white),
+                    )),
+                    CustomPaint(
+                        size: Size.infinite, painter: Path(points: points))
+                  ],
+                ))));
   }
 }
