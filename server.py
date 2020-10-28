c62365be909a54b0532be15a8accb2c4ddc3ded0
Claire Bailie <clarissabailie@gmail.com>
2020-10-28 16:40:55 -0400

    add server.py

---
+++ server.py
@@ -0,0 +1,43 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socketserver
+
+PORT = 8080
+
+class Handler(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        print("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=Handler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...\n')
+    try:
+        print('serving at port ', str(PORT))
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    run()
+
+# Handler = http.server.SimpleHTTPRequestHandler
+
+
+
+# with socketserver.TCPServer(("", PORT), Handler) as httpd:
+#     print("serving at port", PORT)
+#     httpd.serve_forever()
+
